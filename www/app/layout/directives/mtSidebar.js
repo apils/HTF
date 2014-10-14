52ddd8c371fc53e6018bbe9371d4c7fa1fc638f9
apilsworth@sl-x.com <apilsworth@Pils-Laptop.sl-x.com>
2014-10-14 11:11:46 +0100

    First Commit HFT

---
+++ www/app/layout/directives/mtSidebar.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    // TODO: replace app with your module name
+    angular.module('app').directive('mtSidebar', [mtSidebar]);
+    
+    function mtSidebar () {
+        // Usage:
+        // 
+        // Creates:
+        // 
+        var directive = {
+            link: link,
+            restrict: 'A',
+			templateUrl: 'app/layout/sidebar.html'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+        	$('#side-menu').metisMenu();
+        }
+    }
+
+})();
